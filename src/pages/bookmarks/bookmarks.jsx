6bfc71fcec6253e105cc6f1cf4f594451d73ab4f
shobhit-28 <shobhitraj34@gmail.com>
2023-06-24 01:15:57 +0530

    home page ui completed

--- src/pages/bookmarks/bookmarks.jsx
+++ src/pages/bookmarks/bookmarks.jsx
@@ -1,39 +1,38 @@
-import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
-
-import { MdArrowBackIosNew } from 'react-icons/md'
 
 import './bookmarks.css'
 import { PostComponent } from '../../components/postCard/postCardComponent'
 import { PostContext } from '../../contexts/PostContext'
+import { Loader } from '../../components/loader/loader'
 
 export const BookmarkPage = () => {
-    const navigate = useNavigate()
 
     const { bookMarks, allPosts } = useContext(PostContext)
 
     const filteredData = allPosts?.filter((post) => (
-        bookMarks.some((bookmarkedPost) => bookmarkedPost?._id === post?._id )
+        bookMarks.some((bookmarkedPost) => bookmarkedPost?._id === post?._id)
     ))
 
     return (
         <div className="bookmark-page page">
-            <div className="header">
-                <button className="back-btn" onClick={() => navigate(-1)} ><MdArrowBackIosNew /></button>
-                <p className="heading">Bookmarks</p>
-            </div>
-            {bookMarks?.length === 0
-                ?
-                <div className="no-post-div">
-                    <p>No Bookmarks yet</p>
+            {!allPosts ? <Loader /> :
+                <>
+                <div className="header">
+                    <p className="heading">Bookmarks</p>
                 </div>
-                :
-                <div className="post-container">
-                    {filteredData?.map((post) => (
-                        <PostComponent postData={post} key={post?._id} />
-                    ))}
-                </div>
-            }
+                {bookMarks?.length === 0
+                    ?
+                    <div className="no-post-div">
+                        <p>No Bookmarks yet</p>
+                    </div>
+                    :
+                    <div className="post-container">
+                        {filteredData?.map((post) => (
+                            <PostComponent postData={post} key={post?._id} />
+                        ))}
+                    </div>
+                }
+            </>}
         </div>
     )
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,11 +1,57 @@
+import { useContext } from 'react'
+import { useEffect } from 'react'
+
 import './homepage.css'
+import { Loader } from '../../components/loader/loader'
+import { PostComponent } from '../../components/postCard/postCardComponent'
+import { PostContext } from '../../contexts/PostContext'
+import { UserDataContext } from '../../contexts/userDataContext'
 
 export const HomePage = () => {
+    const userId = JSON.parse(localStorage.getItem('userData'))?._id;
+
+    const {allPosts} = useContext(PostContext)
+    const {followed, setFollowed} = useContext(UserDataContext)
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`/api/users/${userId}`)
+            const data = (await response.json())
+            setFollowed(data?.user?.following)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const filteredArr = (allPosts?.filter((post) => (
+        followed?.some((followedPeople) => followedPeople?._id === post?.postedBy?._id )
+    )))
+
+    useEffect(() => {
+        fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <div className="homepage page">
-            <p className="heading">
-                Home
-            </p>
+            {!allPosts ? <Loader /> :
+                <>
+                <div className="header">
+                    <p className="heading">Home</p>
+                </div>
+                {filteredArr?.length === 0
+                    ?
+                    <div className="no-post-div">
+                        <p>Follow your friends to see their latest posts.</p>
+                    </div>
+                    :
+                    <div className="post-container">
+                        {filteredArr?.map((post) => (
+                            <PostComponent postData={post} key={post?._id} />
+                        ))}
+                    </div>
+                }
+            </>}
         </div>
     )
 }
